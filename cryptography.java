e8f28bfdb4b133d0186d95dc6992fd75dec959a9
Benjamin Vachon <77243689+Guildenstern123@users.noreply.github.com>
2021-04-24 00:23:09 -0400

    Add files via upload
    eliminated the need for a character array, and added a CaesarDecrypt function

--- cryptography.java
+++ cryptography.java
@@ -1,23 +1,43 @@
 public class cryptography{
-	static String Caesar(String plaintext, int num){
+	static String CaesarEncrypt(String plaintext, int num){
+		//create string for final cipher text and initialize charNum
 		String cipher = new String("");
-		char[] plainArray = new char[plaintext.length()];
+		int charNum = 0;
 
-		for(int i = 0; i < plaintext.length();i++){
-			plainArray[i] = plaintext.charAt(i);
-		}
+		//add each cipher text character to the cipher variable
+		for(int i = 0; i < plaintext.length(); i++){
+			charNum = (int)plaintext.charAt(i)+num;
 
-		for(int i = 0; i < plainArray.length; i++){
-			if((int)plainArray[i]+num > 122){
-				cipher += (char)(((int)plainArray[i]+num)-97);
+			if(charNum > 'z'){
+				cipher += (char)(charNum-97);
 			}else{
-				cipher += (char)((int)plainArray[i] + num);
+				cipher += (char)charNum;
 			}
 		}
 		return cipher;
 	}
 
+	static String CaesarDecrypt(String ciphertext, int num){
+		//create a string for the final plaintext after decryption & initialize charNum
+		String plaintext = new String("");
+		int charNum = 0;
+
+		//add each plaintext character into the plaintext string
+		for(int i = 0; i < ciphertext.length(); i++){
+			charNum = (int)ciphertext.charAt(i)-num;
+			if(charNum  < 'a'){
+				plaintext += (char)((122+charNum)-122);
+			}else{
+				plaintext += (char)charNum;
+			}
+		}
+
+		return plaintext;
+
+	}
+
 	public static void main(String args[]){
-		System.out.println(Caesar("thisisatest",1));
+		System.out.println(CaesarEncrypt("thisisatest",1));
+		System.out.println(CaesarDecrypt("uijtjtbuftu",1));
 	}
 }
